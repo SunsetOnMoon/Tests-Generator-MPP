bf83a9e9e185d98291fd3d54e900fc7e43f6b9e0
Artsem Kulik <artsem.kulik@gmail.com>
2023-01-06 11:01:58 +0300

    Added tests

---
+++ TestsGenerator.Tests/SourceCode.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator.Tests
+{
+    public static class SourceCode
+    {
+        public const string ClassWithOverridedMethod =
+            @"using System;
+            namespace MyCode
+            {
+                public class MyClass
+                {
+                    public void Method(int a)
+                    {
+                        Console.WriteLine(""Method (int)"");
+                    }
+                    public void Method(double a)
+                    {
+                        Console.WriteLine(""Method (double)"");
+                    }
+                }
+            }";
+
+        public const string ClassWithConstructor =
+            @"using System;
+            namespace MyCode
+            {
+                public class MyClass
+                {
+                    private readonly string _name;
+                    private readonly int _age;
+                    public MyClass(string name, int age)
+                    {
+                        _name = name;
+                        _age = age;
+                    }
+                    public string GetName() => _name;
+                    public int GetAge() => _age;
+                }
+            }";
+
+        public const string ClassWithInterfacePassedInConstructor =
+            @"using System;
+            namespace MyCode
+            {
+                public class MyClass
+                {
+                    private readonly ILol _lol;
+                    public MyClass(ILol lol)
+                    {
+                        _lol = lol;
+                    }
+                    public ILol GetLol() => _lol;
+                }
+            }";
+
+        public const string ClassWithFunction =
+            @"using System;
+            namespace MyCode
+            {
+                public class MyClass
+                {
+                    public string LolToString(ILol lol) => lol.ToString();
+                }
+            }";
+
+        public const string ClassWithProcedure =
+            @"using System;
+            namespace MyCode
+            {
+                public class MyClass
+                {
+                    public void PrintLol(ILol lol) => Console.WriteLine(lol.ToString());
+                }
+            }";
+    }
+}
---
+++ TestsGenerator.Tests/TestHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TestsGenerator.Tests
+{
+    public static class TestHelper
+    {
+        public static List<ClassDeclarationSyntax> GetClasses(string source)
+        {
+            var root = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
+            return root.DescendantNodes()
+                .OfType<ClassDeclarationSyntax>()
+                .ToList();
+        }
+
+        public static ClassDeclarationSyntax GetClass(string source) => GetClasses(source).First();
+
+        public static List<MethodDeclarationSyntax> GetMethods(ClassDeclarationSyntax classSyntax)
+        {
+            return classSyntax.Members.OfType<MethodDeclarationSyntax>().ToList();
+        }
+
+        public static List<FieldDeclarationSyntax> GetFields(ClassDeclarationSyntax classSyntax)
+        {
+            return classSyntax.Members.OfType<FieldDeclarationSyntax>().ToList();
+        }
+    }
+}
